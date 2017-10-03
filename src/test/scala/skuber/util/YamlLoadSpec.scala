fd8d24af05d855e321af8e3b2a78740a153e1a48
David O'Riordan <doriordan@gmail.com>
2017-10-03 07:11:15 +0100

    improved test

--- src/test/scala/skuber/util/YamlLoadSpec.scala
+++ src/test/scala/skuber/util/YamlLoadSpec.scala
@@ -3,6 +3,8 @@ package skuber.util
 import java.io.File
 
 import org.specs2.mutable.Specification
+import skuber.{ObjectResource, Service}
+import skuber.ext.Deployment
 
 /**
   * @author David O'Riordan
@@ -10,7 +12,7 @@ import org.specs2.mutable.Specification
 class YamlLoadSpec extends Specification {
   "This is a unit specification for the skuber util YAML resource loading functionality.\n ".txt
 
-  "The guestbook Yaml can be loaded into a skuber mode" >> {
+  "The guestbook Yaml can be loaded into a skuber model" >> {
 
     val path = getClass.getResource("/guestbook.yaml").getPath
     val file = new File(path)
@@ -23,7 +25,26 @@ class YamlLoadSpec extends Specification {
     val deployments = resources collect { case depl: skuber.ext.Deployment => depl }
     services must haveSize(3)
     deployments must haveSize(3)
-    services exists { svc => svc.name == "redis-master"} must beTrue
-    deployments exists { depl => depl.name == "redis-master"} must beTrue
+    val redisMasterDeploymentOpt=YamlLoadSpec.find[Deployment](resources, "redis-master")
+    redisMasterDeploymentOpt must beSome
+    val redisMasterServiceOpt=YamlLoadSpec.find[Service](resources, "redis-master")
+    redisMasterServiceOpt must beSome
+    val redisSlaveDeploymentOpt=YamlLoadSpec.find[Deployment](resources, "redis-slave")
+    redisSlaveDeploymentOpt must beSome
+    val redisSlaveServiceOpt=YamlLoadSpec.find[Service](resources, "redis-slave")
+    redisSlaveServiceOpt must beSome
+    val frontendDeploymentOpt=YamlLoadSpec.find[Deployment](resources, "frontend")
+    frontendDeploymentOpt must beSome
+    val frontendServiceOpt=YamlLoadSpec.find[Service](resources, "frontend")
+    frontendServiceOpt must beSome
+  }
+}
+
+object YamlLoadSpec {
+  import scala.reflect.ClassTag
+  def find[O <: ObjectResource](resources: List[ObjectResource], resourceName: String)(implicit tag: ClassTag[O]) : Option[O] = {
+    resources.collectFirst[O] {
+      case resource: O if resource.name == resourceName => resource
+    }
   }
 }
